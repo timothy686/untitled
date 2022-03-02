6f2b84192281df87cadbceb7806d670478429f40
timothy686 <98843774+timothy686@users.noreply.github.com>
2022-03-02 11:53:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,15 @@
-function stringvar () {
-    basic.showString("" + (StringVar))
-}
 input.onButtonPressed(Button.A, function () {
     NumberVariable += 6
     basic.showNumber(NumberVariable)
 })
 input.onButtonPressed(Button.B, function () {
-    StringVar += 1
-    stringvar()
+    StringVar = "Hello" + "kii"
+    basic.showString(StringVar)
 })
-let StringVar = 0
+let StringVar = ""
 let NumberVariable = 0
 NumberVariable = 2
 StringVar = "hello"
 let boovar = true
 let spritevar = game.createSprite(2, -2)
 let list = ["hello", "kett"]
-basic.forever(function () {
-	
-})
